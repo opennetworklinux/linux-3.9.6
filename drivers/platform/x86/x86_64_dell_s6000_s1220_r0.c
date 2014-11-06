702c5a2e7c650813d492f6f3598b432cc5aaa648
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-11-06 10:05:38 -0800

    Additional variants.

--- drivers/platform/x86/x86_64_dell_s6000_s1220_r0.c
+++ drivers/platform/x86/x86_64_dell_s6000_s1220_r0.c
@@ -87,7 +87,8 @@ static int __init x86_64_dell_s6000_s1220_r0_init(void)
     product = dmi_get_system_info(DMI_PRODUCT_NAME);
 
     if(strcmp(vendor, "Dell Inc") ||
-       (strcmp(product, "S6000 (SI)") && strcmp(product, "S6000-ON"))) {
+       (strcmp(product, "S6000 (SI)") && strcmp(product, "S6000-ON") &&
+        strcmp(product, "S6000-ON (SI)"))) {
         /* Not the S6000 */
         return -ENODEV;
     }
